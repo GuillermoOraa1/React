6ee13456507c7985ee68541f7e61926a162d82af
GuillermoOraa1 <retaziles@hotmail.com>
2023-05-03 23:18:02 +0200

    commit con controles de regiones y paises

--- proyecto-mapa-iucn/src/components/header/Header.jsx
+++ proyecto-mapa-iucn/src/components/header/Header.jsx
@@ -1,9 +1,41 @@
+import IUCNlogo from "../../assets/images/IUCN_Red_List-2.png";
+import amphibiansImage from "../../assets/icons/amphibians.svg";
+import mammalsImage from "../../assets/icons/mammals.svg";
+import birdsImage from "../../assets/icons/birds.svg";
+import reptilesImage from "../../assets/icons/reptile.svg";
+import fishesImage from "../../assets/icons/fishes.svg";
+import insectsImage from "../../assets/icons/insects.svg";
+import './Header.css';
 
 const header =()=>{
     return(
-        <>
-            <h1>Esto es el header</h1>
-        </>
+        <div className="header">
+            <div className="logo">
+                <img src={IUCNlogo} alt="IUCN logo" width="160px" height="160px" />
+            </div>
+            <div className="text">
+                <div className="title">
+                    <h1>The IUCN Red List of Threatened Species</h1>
+                </div>
+                <div className="listOfSpecies">
+                    <div className="listOfSpecies-text">
+                        <h3>More than 42,100 species are threatened with extinction.</h3>
+                        <h4>That is still 28% of all assessed species.</h4>
+                    </div>
+                    <div className="listOfSpecies-icons">
+                        <p>Amphibians <span><strong>41%</strong> <img src={amphibiansImage} alt="Amphibians icon" /></span></p>
+                        <p>Mammals <span><strong>27%</strong> <img src={mammalsImage} alt="Mammals icon"  /></span></p>
+                        <p>Birds <span><strong>13%</strong> <img src={birdsImage} alt="Birds icon"  /></span></p>
+                        <p>Reptiles <span><strong>21%</strong> <img src={reptilesImage} alt="Reptiles icon" /></span></p>
+                        <p>Fishes <span><strong>55%</strong> <img src={fishesImage} alt="Fishes icon" /></span></p>
+                        <p>Insects <span><strong>40%</strong> <img src={insectsImage} alt="Insects icon" /></span></p> 
+                    </div>
+                </div>
+            </div>
+
+            
+
+        </div>
     );
 }
 
--- proyecto-mapa-iucn/src/components/map/Map.jsx
+++ proyecto-mapa-iucn/src/components/map/Map.jsx
@@ -15,119 +15,172 @@ var countryGroups=L.featureGroup([]);
 var paisSeleccionado=null;
 var zoom1;
 
+
+
 const RefMap = () => {
+    const {results,paises}=data;
+
+    function cargarSelectPaises(identificadorRegion){
+      var selectPaises=document.getElementById("select-pais-id");
+      const {paises}=data;
+      paises.forEach(pais => {
+        if(pais.region === identificadorRegion){
+          var opt = document.createElement('option');
+          opt.value = pais.zoom+","+pais.coordinates[0]+","+pais.coordinates[1];
+          opt.innerHTML=pais.country;
+          selectPaises.appendChild(opt);
+        }
+      });
+    } 
 
     
     //CREAMOS LA VARIABLE QUE HACE REFERENCIA AL MAPA
     const refMap = useMap();
-    useEffect(() => {console.log("sss")},[refMap]);
-    
-    //METEMOS LOS DATOS DEL GEOJSON
-    var fronteras=L.geoJSON(countries_geojson,{
-      style: function (feature) {
-          return {fillColor: 'none',
-          fillOpacity: 0,};
-      }
-    });
-    fronteras.addTo(refMap);
-    
-    //RELLENAMOS EL SELECT DE LAS REGIONES
-    const {results,paises}=data;
-    var selectRegiones=document.getElementById("select-region-id");
-    results.forEach(region => {
-            var opt = document.createElement('option');
-            opt.id=region.identifier;
-            opt.value = region.zoom+","+region.coordinates[0]+","+region.coordinates[1];
-            opt.innerHTML=region.name;
-            selectRegiones.appendChild(opt);
-    });
 
-    //AGREGAMOS LOS MARCADORES DE LAS REGIONES
-    results.map((point)=>{
-        if(point.name!=="Global"){
-          var marcador=L.marker([point.coordinates[0],point.coordinates[1]]).bindTooltip(point.name,{direction:'center',permanent:true}).openTooltip();
-          marcador.on('click', ()=>{refMap.flyTo([point.coordinates[0],point.coordinates[1]],point.zoom)});
-          marcador.addTo(regionsGroups);
-        }
-        return null;
-	  });
-    regionsGroups.addTo(refMap);
+    const [omap, setOMap] = useState();
 
-    //AGREGAMOS LOS MARCADORES DE LOS PAISES  
-    paises.map((pais)=>{
-      var marcador=L.marker([pais.coordinates[0],pais.coordinates[1]]).bindTooltip(pais.country,{direction:'center',permanent:true}).openTooltip();
-      marcador.on('click', ()=>{
-          refMap.flyTo([pais.coordinates[0],pais.coordinates[1]],pais.zoom);
-          //mostrarAnimalClassControl(false);
-          paisSeleccionado=pais;
-          //fetchDataByCountry (pais);                
-          refMap.on('zoomend', function() {
-              if(refMap.getZoom() === pais.zoom) {
-                  zoom1=pais.zoom;
+    useEffect(() => {
+                setOMap(refMap);
+        }, [refMap])
+    
+    useEffect(() => {
+        if (omap) {
+              //METEMOS LOS DATOS DEL GEOJSON
+            var fronteras=L.geoJSON(countries_geojson,{
+              style: function (feature) {
+                  return {fillColor: 'none',
+                  fillOpacity: 0,};
               }
-          });
-          
-      });
-      marcador.addTo(countryGroups);
-      marcador.on('contextmenu', function() { 
-          //mostrarAnimalClassControl(false);
-          
-      });
-      return null;
-    });
+            });
+            fronteras.addTo(omap);
+    
+            
 
-    //PONEMOS Y QUITAMOS CAPAS EN FUNCION DEL ZOOM
-    refMap.on('zoomend', function() {
-      if(refMap.getZoom() >8 && refMap.hasLayer(fronteras)){
-        refMap.removeLayer(fronteras);
-      }
-      if(refMap.getZoom() <8 && !refMap.hasLayer(fronteras)){
-          fronteras.addTo(refMap); 
-      }
-      if(refMap.getZoom() <(zoom1-2)){
-          //mostrarAnimalClassControl(false);
-      }
-    if (refMap.getZoom() <4){
-          refMap.removeLayer(countryGroups);
-          regionsGroups.addTo(refMap);
-      }else {
-          refMap.removeLayer(regionsGroups);
-          countryGroups.addTo(refMap);       
-      }
-    });
 
-    //CONTROL PARA HACER FULL SCREEN
-    var fullScreenControl = L.control({position:'topright'});
-    fullScreenControl.onAdd=()=>{
-        const container = L.DomUtil.create("input", "leaflet-control-zoom leaflet-bar leaflet-control");
-        container.type = "button";
-        container.title = "Ver en pantalla completa";
-        container.style.backgroundImage = "url(https://cdn-icons-png.flaticon.com/512/2089/2089670.png)";
-        container.style.backgroundSize = "15px 15px";
-        container.style.backgroundRepeat = "no-repeat";
-        container.style.backgroundPosition = "50% 50%";
-        container.style.width = "32px";
-        container.style.height = "32px";
-        container.style.padding = "12px";
-        container.style.lineHeight = "30px";
-        container.style.fontSize = "22px";
-        container.style.fontWeight = "bold";
-        container.style.cursor = "pointer";
+            //RELLENAMOS EL SELECT DE LAS REGIONES Y LES ASIGNAMOS ACCIONES
+            var selectRegiones=document.getElementById("select-region-id");
+            results.forEach(region => {
+                    var opt = document.createElement('option');
+                    opt.id=region.identifier;
+                    opt.value = region.zoom+","+region.coordinates[0]+","+region.coordinates[1];
+                    opt.innerHTML=region.name;
+                    selectRegiones.appendChild(opt);
+            });
+            document.getElementById("select-region-id").addEventListener('change',(event)=>{
+                let ZoomYCoordenadas= event.target.value.split(",");
+                omap.flyTo([ZoomYCoordenadas[1],ZoomYCoordenadas[2]],ZoomYCoordenadas[0]);
+                document.getElementById("select-pais-id").innerHTML="";
+                cargarSelectPaises(event.target.options[event.target.selectedIndex].id); //Rellenamos el select de los paises
+              }); 
 
-        container.onclick = () => {
-            if (!document.fullscreenElement) {
-                document.getElementById("mapa")?.requestFullscreen();
-                container.title = "Salir de pantalla completa";
-            }else{
+            //DESPUES DE RELLENAR EL SELECT DE LOS PAISES LES ASIGNAMOS ACCIONES            
+            document.getElementById('select-pais-id').addEventListener('change',(event)=>{
+              let ZoomYCoordenadas= event.target.value.split(",");
+              omap.flyTo([ZoomYCoordenadas[1],ZoomYCoordenadas[2]],ZoomYCoordenadas[0]);
+            });
+
+            //AGREGAMOS LOS MARCADORES DE LAS REGIONES
+            results.map((point)=>{
+                if(point.name!=="Global"){
+                  var marcador=L.marker([point.coordinates[0],point.coordinates[1]]).bindTooltip(point.name,{direction:'center',permanent:true}).openTooltip();
+                  marcador.on('click', ()=>{omap.flyTo([point.coordinates[0],point.coordinates[1]],point.zoom)});
+                  marcador.addTo(regionsGroups);
+                }
+                return null;
+              });
+            regionsGroups.addTo(omap);
+
+           //AGREGAMOS LOS MARCADORES DE LOS PAISES  
+            paises.map((pais)=>{
+              var marcador=L.marker([pais.coordinates[0],pais.coordinates[1]]).bindTooltip(pais.country,{direction:'center',permanent:true}).openTooltip();
+              marcador.on('click', ()=>{
+                  omap.flyTo([pais.coordinates[0],pais.coordinates[1]],pais.zoom);
+                  //mostrarAnimalClassControl(false);
+                  paisSeleccionado=pais;
+                  //fetchDataByCountry (pais);                
+                  omap.on('zoomend', function() {
+                      if(omap.getZoom() === pais.zoom) {
+                          zoom1=pais.zoom;
+                      }
+                  });
+                  
+              });
+              marcador.addTo(countryGroups);
+              marcador.on('contextmenu', function() { 
+                  //mostrarAnimalClassControl(false);
+                  
+              });
+              return null;
+            });
+
+            //PONEMOS Y QUITAMOS CAPAS EN FUNCION DEL ZOOM
+            omap.on('zoomend', function() {
+              if(omap.getZoom() >8 && omap.hasLayer(fronteras)){
+                omap.removeLayer(fronteras);
+              }
+              if(omap.getZoom() <8 && !omap.hasLayer(fronteras)){
+                  fronteras.addTo(omap); 
+              }
+              if(omap.getZoom() <(zoom1-2)){
+                  //mostrarAnimalClassControl(false);
+              }
+            if (omap.getZoom() <4){
+                  omap.removeLayer(countryGroups);
+                  regionsGroups.addTo(omap);
+              }else {
+                  omap.removeLayer(regionsGroups);
+                  countryGroups.addTo(omap);       
+              }
+            });
+
+            //CONTROL PARA HACER FULL SCREEN
+            var fullScreenControl = L.control({position:'topright'});
+            fullScreenControl.onAdd=()=>{
+                const container = L.DomUtil.create("input", "leaflet-control-zoom leaflet-bar leaflet-control");
+                container.type = "button";
                 container.title = "Ver en pantalla completa";
-                document.exitFullscreen();
+                container.style.backgroundImage = "url(https://cdn-icons-png.flaticon.com/512/2089/2089670.png)";
+                container.style.backgroundSize = "15px 15px";
+                container.style.backgroundRepeat = "no-repeat";
+                container.style.backgroundPosition = "50% 50%";
+                container.style.width = "32px";
+                container.style.height = "32px";
+                container.style.padding = "12px";
+                container.style.lineHeight = "30px";
+                container.style.fontSize = "22px";
+                container.style.fontWeight = "bold";
+                container.style.cursor = "pointer";
+
+                container.onclick = () => {
+                    if (!document.fullscreenElement) {
+                        document.getElementById("mapa")?.requestFullscreen();
+                        container.title = "Salir de pantalla completa";
+                    }else{
+                        container.title = "Ver en pantalla completa";
+                        document.exitFullscreen();
+                    }
+                };
+                return container;
             }
-        };
-        return container;
-    }
-    fullScreenControl.addTo(refMap);
+            fullScreenControl.addTo(omap);
+
+            
+          
+
+        }
+    }, [omap]);
+    
+
+/* 
 
 
+
+
+
+
+
+
+ */
 
 
     return null;
@@ -139,8 +192,6 @@ const RefMap = () => {
  
 
 const Mapa =()=>{
-
-
 
     return (
       <>
